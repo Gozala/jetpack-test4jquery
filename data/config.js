80491762262246e6e681253a9999ff71b1f3addc
Irakli Gozalishvili <rfobic@gmail.com>
2011-04-18 18:14:27 +0200

    Adding another workaround for tests that check `location.protocol`.

--- data/config.js
+++ data/config.js
@@ -1,2 +1,10 @@
 // Letting jQuery now that there is no server around.
+Object.defineProperties(location, {
+  // Selector test for example use if statements with condition
+  // `location.protocol != "file:"` to define some test that make no sense when
+  // they run outside of `http:` or `https:` so we override `resource:` to get
+  // desired evaluation of condition in those test.
+  protocol: { value: "file:" }
+});
+// Letting jQuery now that there is no server around.
 isLocal = true;
